4dc5f8aecd15118d82432810d2c6a8befb5d2996
RehabBadwy <rehabbadwy0@gmail.com>
2021-08-08 16:51:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import Header from './Component/Ui/Header';
+import CharacterGrid from './Component/Characters/CharacterGrid';
+import Search from './Component/Ui/Search';
 
-function App() {
+const App =()=> {
+
+  const [items, setItems] = useState ([])
+  const [isLoading, setIsLoading] = useState (true)
+
+
+  useEffect( () =>{
+    const fetchItems = async () => {
+      const result = await axios ('https://www.breakingbadapi.com/api/characters?')
+
+      console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+     
+    }
+    fetchItems()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header />
+      <Search />
+      <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
---
+++ src/Component/Ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../../img/logo.png'
+
+
+function Header() {
+    return (
+        <header className="center">
+            <img src={Logo} />
+        </header>
+    )
+}
+
+export default Header
